8c6ca22c25c4e8b534c26c72b5b463a87b7a65db
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-07-29 13:55:55 +0200

    add composant bootstrap test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ngbd-carousel-navigation',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
-export class AppComponent {
-  title = 'collegues-front';
+export class NgbdCarouselNavigation {
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
+  images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+  }
 }
